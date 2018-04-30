de8bfe9581fb6510803b4fd27415db277eaadfe2
Daniel <daan@compagner.com>
2018-04-30 18:42:05 +0200

    First Commit

---
+++ Spelletje/Spelletje/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+namespace Spelletje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoi Wouter");
+            Console.Read();
+        }
+    }
+}
