1511355f5b7aa7cd04cba3575e0aacfa730e5d72
levaritie <wouterhidding@hotmail.nl>
2018-04-30 19:14:21 +0100

    hello world

--- Spelletje/Spelletje/Program.cs
+++ Spelletje/Spelletje/Program.cs
@@ -8,6 +8,7 @@ namespace Spelletje
         static void Main(string[] args)
         {
             Console.WriteLine("Hoi Wouter");
+            Console.WriteLine("Hoi Daniel");
             Console.Read();
         }
     }
